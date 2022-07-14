c7270465e60460859245276ae43a838f61291968
Utkarsh Tiwari <79445012+UT-03@users.noreply.github.com>
2022-07-14 11:49:51 +0530

    feat: add contacts screen

---
+++ screens/ContactsScreen.js
@@ -0,0 +1,32 @@
+import { useContext } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+import { Text } from 'react-native-paper'
+import { ContactsContext } from '../context/ContactsContext';
+import { StatusBar } from 'expo-status-bar';
+import ContactItem from '../components/ContactItem';
+
+const ContactsScreen = () => {
+    const { contacts, areContactsReady } = useContext(ContactsContext);
+
+    return (
+        <>
+            <StatusBar style="light" />
+            {
+                areContactsReady ? (
+                    contacts.map((contact, index) => {
+                        return (
+                            <ContactItem
+                                key={contact._id}
+                                _id={contact._id}
+                                name={contact.name} />
+                        )
+                    })
+                ) : (
+                    <ActivityIndicator />
+                )
+            }
+        </>
+    );
+};
+
+export default ContactsScreen;
